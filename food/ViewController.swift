e6f055c1cdea5499546e134661711ed232046f2b
Arushi Virmani <avirma4@uic.edu>
2021-07-22 10:12:57 -0500

    Initial Commit

---
+++ food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food
+//
+//  Created by Arushi Virmani on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
